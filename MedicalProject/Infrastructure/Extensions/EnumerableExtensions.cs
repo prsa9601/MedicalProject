07f0c92332477f9b3e7b7eed7697e51bc14c1ef9
mohammad parsa karimi <parsa9601m@outlook.com>
2025-11-16 20:38:39 +0330

    a

---
+++ MedicalProject/Infrastructure/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalProject.Infrastructure.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static decimal SafeSum<T>(this IEnumerable<T> source, Func<T, string> selector)
+        {
+            return source
+                .Select(item => selector(item))
+                .Where(priceStr => decimal.TryParse(priceStr, out _))
+                .Sum(priceStr => decimal.Parse(priceStr));
+        }
+    }
+}
--- MedicalProject/Pages/Admin/Finance/Index.cshtml.cs
+++ MedicalProject/Pages/Admin/Finance/Index.cshtml.cs
@@ -1,12 +1,27 @@
+using MedicalProject.Infrastructure.RazorUtils;
+using MedicalProject.Models.PurchaseReport;
+using MedicalProject.Services.PurchaseReport;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace MedicalProject.Pages.Admin.Finance
 {
-    public class IndexModel : PageModel
+    public class IndexModel : BaseRazorFilter<PurchaseReportFilterParam>
     {
-        public void OnGet()
+        private readonly IPurchaseReportService _service;
+
+        public IndexModel(IPurchaseReportService service)
         {
+            _service = service;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public PurchaseReportFilterResult? Result { get; set; }
+        
+        public async Task OnGet(CancellationToken cancellationToken)
+        {
+            Result = await _service.GetFilterForAdmin(FilterParams, cancellationToken);
         }
     }
 }
