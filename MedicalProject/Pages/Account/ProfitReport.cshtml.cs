6704b5d4047a8c9d07fa9448ee978870633f6a48
dragon <parsa9601m@gmail.com>
2025-11-26 14:59:39 +0330

    fews

---
+++ MedicalProject/Pages/Account/ProfitReport.cshtml.cs
@@ -0,0 +1,27 @@
+using MedicalProject.Infrastructure.RazorUtils;
+using MedicalProject.Models.PurchaseReport;
+using MedicalProject.Services.PurchaseReport;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace MedicalProject.Pages.Account
+{
+    public class ProfitReportModel : BaseRazorFilter<UserPurchaseReportFilterParam>
+    {
+        private readonly IPurchaseReportService _service;
+
+        public ProfitReportModel(IPurchaseReportService service)
+        {
+            _service = service;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public UserPurchaseReportDto Result { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            Result = await _service.GetForCurrentUser();
+            return Page();
+        }
+    }
+}
