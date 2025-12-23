0c867ca294cbb4961c6e0515186b455172a81afe
dragon <parsa9601m@gmail.com>
2025-12-24 01:05:22 +0330

    pllp

--- MedicalProject/Models/Order/OrderFilterData.cs
+++ MedicalProject/Models/Order/OrderFilterData.cs
@@ -5,7 +5,7 @@
         public DateTime DateOfPurchase { get; set; }
         public Guid UserId { get; set; }
         public OrderStatus status { get; set; }
-        public List<OrderItemDto> OrderItems { get; set; }
+        public OrderItemDto? OrderItems { get; set; }
     }
     public class OrderFilterParam : BaseFilterParam
     {
--- MedicalProject/Pages/Admin/Order/Index.cshtml.cs
+++ MedicalProject/Pages/Admin/Order/Index.cshtml.cs
@@ -1,12 +1,28 @@
+using MedicalProject.Infrastructure.RazorUtils;
+using MedicalProject.Models.Order;
+using MedicalProject.Services.Order;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace MedicalProject.Pages.Admin.Order
 {
-    public class IndexModel : PageModel
+    public class IndexModel : BaseRazorFilter<OrderFilterParam>
     {
-        public void OnGet()
+        private readonly IOrderService _service;
+
+        public IndexModel(IOrderService service)
         {
+            _service = service;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public OrderFilterResult Order { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Order = await _service.GetFilter(FilterParams, CancellationToken.None);
+            return Page();
         }
     }
 }
